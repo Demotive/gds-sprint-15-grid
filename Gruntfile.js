00bc4f6d132936c2f641dc128b192f573dd12d64
Demotive <mat@demotive.com>
2015-01-15 10:05:47 +0000

    Sass

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+
+  // Default task(s).
+  grunt.registerTask('default', ['']);
+
+};
