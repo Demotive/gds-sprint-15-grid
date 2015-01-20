e3ad91c8143cd1791829ec67e5a736b455e490b0
Demotive <mat@demotive.com>
2015-01-20 14:20:18 +0000

    New colour scheme, alternate 12 layout etc

--- public/js/application.js
+++ public/js/application.js
@@ -1,3 +1,4 @@
+var flipCards = [];
 
 var flipCard = function($card) {
   var rotation = $card.data('rotation');
@@ -5,18 +6,45 @@ var flipCard = function($card) {
   $card.css({
     transform: 'rotateY(' + (rotation) + 'deg)'
   });
+  /*$card.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+    console.log('flipped');
+  });*/
   $card.data('rotation', rotation);
+}
+
+var flipNextCard = function(i) {
+  if (i === flipCards.length) {
+    return false;
+  }
+  flipCard(flipCards[i]);
+  flipCards[i].one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend', function(e) {
+    i++;
+    flipNextCard(i);
+  });
 }
 
 var init = function() {
 
   $('.flip-card').each(function() {
-    $(this).data('rotation', 0);
+    $this = $(this);
+    $this.data('rotation', 0);
+    flipCards.push($this);
   });
 
   // make this a random timer call
   $(document).on('click', '.flip-card', function() {
     flipCard($(this));
+  });
+
+  // try this
+  $(document).on('click', '.sprint-logo', function() {
+    var i = 0;
+    flipNextCard(i);
+    // All cards at once
+    /*for (var i=0; i<flipCards.length; i++) {
+      var current = flipCards[i];
+      flipCard(current);
+    }*/
   });
 
 };
