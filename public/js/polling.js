5d50315703509f9e17109192c121ec21d91a1a77
Demotive <mat@demotive.com>
2015-01-22 16:58:30 +0000

    Split out live data polling into separate script

---
+++ public/js/polling.js
@@ -0,0 +1,67 @@
+var realtimeServices = [
+  {
+    url: '/govuk/realtime',
+    selector: '#govuk'
+  },
+  {
+    url: '/driving-test/realtime',
+    selector: '#driving-test'
+  },
+  {
+    url: '/blood-donor-appointments/realtime',
+    selector: '#blood-donor-appointments'
+  },
+  {
+    url: '/prison-visits/realtime',
+    selector: '#prison-visits'
+  },
+  {
+    url: '/carers-allowance/realtime',
+    selector: '#carers-allowance'
+  },
+  {
+    url: '/tax-disc/realtime',
+    selector: '#tax-disc'
+  },
+  {
+    url: '/view-driving-record/realtime',
+    selector: '#view-driving-record'
+  },
+  {
+    url: '/registered-traveller-scheme/realtime',
+    selector: '#registered-traveller-scheme'
+  },
+  {
+    url: '/register-to-vote/realtime',
+    selector: '#register-to-vote'
+  },
+  {
+    url: '/sorn/realtime',
+    selector: '#sorn'
+  },
+  {
+    url: '/licensing/realtime',
+    selector: '#licensing'
+  }
+];
+
+var updateUsers = function() {
+  for (var i=0; i<realtimeServices.length; i++) {
+    $.ajax({
+      dataType: 'json',
+      cache: false,
+      async: false,
+      url: realtimeServices[i].url,
+      success: function(d) {
+        $(realtimeServices[i].selector).text(addCommas(d));
+      }
+    });
+  }
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+$(function() {
+  updateUsers();
+  var pollServices = window.setInterval(updateUsers, 30*1000);
+});
